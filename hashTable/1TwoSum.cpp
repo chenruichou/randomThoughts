61c55d57e05654136da86f9dd0c0961759817880
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2015-08-20 00:39:41 -0700

    Added the first few lines...

--- hashTable/1TwoSum.cpp
+++ hashTable/1TwoSum.cpp
@@ -1,2 +1,15 @@
 #include<stdio.h>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
 
+
+int main(){
+	
+	return 0;
+}
