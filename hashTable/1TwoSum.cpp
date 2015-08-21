fc24924bfae8e294cc71ffbe8ff875a8e48a0744
chenruichou <chenrui.chou@gmail.com>
2015-08-20 17:34:16 -0700

    Added a test case

--- hashTable/1TwoSum.cpp
+++ hashTable/1TwoSum.cpp
@@ -4,12 +4,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+	vector<int> out;
+	return out;        
     }
 };
 
 
 int main(){
-	
+	int nums[] = {2,3,4,5,6,7,8};
+	vector<int> nums_v;
+	nums_v.assign(nums,nums+6);
+	Solution s;	
+	int tar=10;
+	vector <int> out = s.twoSum(nums_v,tar);
 	return 0;
 }
