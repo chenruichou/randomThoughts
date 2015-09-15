2a00b5bef36456d905a0d76946e95eb1b67b2177
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2015-09-14 21:08:08 -0700

    Added no.14

--- string/14longestCommonPrefix.cpp
+++ string/14longestCommonPrefix.cpp
@@ -6,15 +6,28 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string out = "";
-	for (auto it=strs.begin();it!=strs.end();it++){
-		
+	int d=0;
+	bool is_quit=0;
+	while (!is_quit){
+		char c = (strs[0])[d];
+		for (auto it=strs.begin()+1;it!=strs.end();it++){
+			if((*it).size()<d+1 || (*it)[d]!=c)
+				is_quit = 1;		
+		}
+		if(!is_quit)
+			d++;
 	}
+	return (strs[0]).substr(0,d);
     }
 };
 
 int main(){
-	vector<string> strs = {"hello","hallow","half","happy","ello","alf","ape"};
-		
+	vector<string> strs = {"hallo","hallow","half"};
+	Solution sol;
+	string out = sol.longestCommonPrefix(strs);
+	if(!out.empty())
+		cout<<out<<endl;
+	else
+		cout<<"Nothing matched"<<endl;	
 	return 1;
 }
