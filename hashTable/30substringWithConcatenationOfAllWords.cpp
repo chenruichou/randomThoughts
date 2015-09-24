5549919f8113551e0832c35df9f22e198a9e9779
chenruichou <chenrui.chou@gmail.com>
2015-09-23 18:03:57 -0700

    Added unfinished no.30

---
+++ hashTable/30substringWithConcatenationOfAllWords.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+			            
+    }
+};
+
+int main(){
+	Solution sol;
+	sol.findSubstring();
+
+	return 1;
+}
