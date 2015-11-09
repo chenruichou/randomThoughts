0d2c145d82c04616dc6babae7ced055c459de25d
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2015-11-08 21:15:27 -0800

    Modified no.38

--- string/38CountAndSay.cpp
+++ string/38CountAndSay.cpp
@@ -7,20 +7,18 @@ class Solution{
 public:
 	string countAndSay(int n){
 		string s="1";
-		string s_new="";
 		if(n<1) return "";
 		int d = 1;
-		char prev = s[0];
-		char curr = 'x';
 		while(d<=n){
+			string s_new = "";
+			char prev = s[0];
 			int count = 1;
-			for(auto it=s.begin();it!=s.end();it++){
-				cout<<*it<<endl;
-				curr = *it;
+			for(auto it=s.begin()+1;it!=s.end();it++){
+				char curr = *it;
 				if(curr==prev){
 					count++;
 				}else{
-					s_new.push_back((char)('0'+count));
+					s_new.push_back((char) '0'+count);
 					s_new.push_back(prev);
 					
 					// reset
@@ -28,9 +26,11 @@ public:
 					prev = curr;
 				}
 			}
-			cout<<s_new<<endl;
+			// push the last
+			s_new.push_back((char) '0'+count);
+			s_new.push_back(prev);
 			s = s_new;
-			s_new.clear();
+			cout<<s_new<<endl;
 			d++;
 		}
 		return s;
@@ -42,7 +42,7 @@ public:
 int main(){
 	string s = "1";
 	Solution sol;
-	int n=3;
+	int n=10;
 	sol.countAndSay(n);
 	return 1;
 }
