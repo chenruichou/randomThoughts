04be3e9685d47fc5c6a1912f38e9ed774bf162f9
chenruichou <chenrui.chou@gmail.com>
2015-11-09 20:32:48 -0800

    Added no.41

--- array/41findMissingPositive.cpp
+++ array/41findMissingPositive.cpp
@@ -7,19 +7,23 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
 	for(int i=0; i<nums.size();i++){
-		while(nums[i]!=i+1) { // put 1 at index 0
+		if(nums[i]!=i) { // put 1 at index 0
 			//SKIP for those out of bound or already correct
 			if(nums[i] <= 0 || nums[i] >nums.size() || nums[i] == nums[nums[i] -1]) break;   
-			//SWAP
+			//Index to swap to
+			int indx=nums[i]-1;
+			if(indx<0) indx=0;
+			if(indx>nums.size()-1) indx=nums.size()-1;
+			//Swap
 			int temp = nums[i];
-			nums[i] = nums[nums[i]-1];
-			nums[nums[i]-1] = temp;
+			nums[i] = nums[indx];
+			nums[indx] = temp;
 		}
 	}
 	//Scan again to detect the missing hole
 	for(int i=0;i<nums.size();i++){
-		if(nums[i]!=i+1)
-			return i+1;
+		if(nums[i]>0 && nums[i]!=i)
+			return i;
 	}
 	return nums.size();
         
@@ -28,7 +32,7 @@ public:
 
 int main(){
 	Solution sol;
-	vector<int> nums = {3,4,-1,1};
+	vector<int> nums = {5,1,3,4,0};
 	cout << sol.firstMissingPositive(nums)<< endl;
 
 	return 1;
