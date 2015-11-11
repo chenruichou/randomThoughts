58619c2665e3289824014ac29c0e89e71d3c3be3
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2015-11-10 20:59:50 -0800

    Added no.47

---
+++ backTracking/47permutation2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution{
+	public:
+		vector<vector<int>> permutation(vector<int> &nums){
+			vector<vector<int>> out;
+			//out.push_back(nums); //push the original
+			findPermutation(nums, 0, out);
+			return out;
+		}
+		void findPermutation(vector<int> &nums, int indx, vector<vector<int>> &out){
+			if(indx==nums.size()-1) return;
+			int start;
+			if(indx==0)
+				start = 0; // tricky!  To special process the first case
+			else
+				start = indx+1;
+			
+			for(int i=start; i<nums.size();i++){
+				vector<int> nums_copy(nums);
+				//swap from "indx" to "i"
+				if(start==0 || nums_copy[i]!=nums_copy[indx]){
+					int temp = nums_copy[indx];
+					nums_copy[indx] = nums_copy[i];
+					nums_copy[i] = temp;
+					out.push_back(nums_copy);
+					findPermutation(nums_copy, indx+1, out);
+				}
+			}
+			return ;
+		}
+};
+
+int main(){
+	Solution sol;
+	vector<int> nums = {1,2,3};
+	vector<vector<int>> out = sol.permutation(nums);
+	for(int i=0; i<out.size();i++){
+		for(int j=0;j<out[i].size();j++){
+			cout << out[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 1;
+}
