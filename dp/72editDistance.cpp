2f1a169823236615c37fc43b742d611cfe59ab9a
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2016-01-13 23:14:39 -0800

    Added no.72

--- dp/72editDistance.cpp
+++ dp/72editDistance.cpp
@@ -3,29 +3,32 @@
 using namespace std;
 
 class Solution {
-    public:
-        int minDistance(string word1, string word2) {
-            int n1 = word1.size();
-            int n2 = word2.size();
-            int out = solMinDistance(word1, word2, 0, 0, n1, n2);
-        }
-        int solMinDistance(string word1, string word2, int i, int j, int n1, int n2){
-            if(i==n1-1 && j==n2-1){
-                return 1;
-            }else{
-                // not matched => Edit!
-                
-            
-            }
-        
-        }
+	public:
+		int minDistance(string word1, string word2) {
+			int out = solMinDistance(word1, word2, 0, 0);
+			return out;
+		}
+		int solMinDistance(string word1, string word2, int i, int j){
+			if(i==word1.size()-1){
+				return word2.size()-j-1;
+			}
+			if(j==word2.size()-1){
+				return word1.size()-i-1;
+			}
+			
+			int addDist = solMinDistance(word1,word2,i,j+1)+1;
+			int remvDist = solMinDistance(word1,word2,i+1,j)+1;
+			int chgDist = solMinDistance(word1,word2,i+1,j+1) + (word1[i]==word2[j] ? 0 : 1);
+
+			return min(min(addDist,remvDist),chgDist);
+		}
 };
 
 int main(){
-    Solution sol;
-    string w1 = "hellobabe";
-    string w2 = "hallowbeyourname";
-    int out = sol.minDistance(w1,w2);
-    cout << out << endl;
-    return 1;
+	Solution sol;
+	string w1 = "hello";
+	string w2 = "hallowbeyourname";
+	int out = sol.minDistance(w1,w2);
+	cout << out << endl;
+	return 1;
 }
