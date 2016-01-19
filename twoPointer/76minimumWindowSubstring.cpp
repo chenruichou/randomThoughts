ef8ea9286b095dd59602e4fc31ce31c464f1d10b
chenruichou <chenrui.chou@gmail.com>
2016-01-19 00:18:11 -0800

    Added no.76

--- twoPointer/76minimumWindowSubstring.cpp
+++ twoPointer/76minimumWindowSubstring.cpp
@@ -6,7 +6,20 @@ using namespace std;
 class Solution {
 public:
     string minWindow(string s, string t) {
+        unordered_map<char, int> m;
         
+        // scan for the target set
+        for(int i=0;i<t.size();i++){
+            m[t[i]]=1; // build hashtable
+        }
+
+        unordered_map<char, int> n;
+
+        // scan for the source set
+        for(int i=0; i<s.size();i++){
+            if(m[s[i]]==1)
+                n[s[i]]++;
+        }
     }
 };
 
