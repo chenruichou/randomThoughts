18ee70b5f7eba23d8a1f23def982d941d0dd2dcf
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2016-01-18 22:22:04 -0800

    Added no.76 (partial)

---
+++ twoPointer/76minimumWindowSubstring.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+    }
+};
+
+
+int main(){
+	Solution sol;
+	string s="hello";
+	string t="eo";
+	string out = sol.minWindow(s,t);
+	cout << out << endl;
+	return 1;
+}
