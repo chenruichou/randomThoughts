365a5e231a9522b2eb0e31f2a73be042f32e8cf9
chenruichou <chenrui.chou@gmail.com>
2016-01-20 00:21:20 -0800

    Added no.76

--- twoPointer/76minimumWindowSubstring.cpp
+++ twoPointer/76minimumWindowSubstring.cpp
@@ -1,34 +1,74 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
-
 class Solution {
-public:
-    string minWindow(string s, string t) {
-        unordered_map<char, int> m;
-        
-        // scan for the target set
-        for(int i=0;i<t.size();i++){
-            m[t[i]]=1; // build hashtable
-        }
+    public:
+        string minWindow(string s, string t) {
+            if(s=="") return "";
+            if(t=="") return "";
 
-        unordered_map<char, int> n;
+            unordered_map<char, int> m;
+            unordered_map<char, int> n;
 
-        // scan for the source set
-        for(int i=0; i<s.size();i++){
-            if(m[s[i]]==1)
-                n[s[i]]++;
+            // scan for the target set
+            for(int i=0;i<t.size();i++){
+                m[t[i]]=1; // build hashtable
+            }
+
+            // scan for the source set
+            int left=0;
+            int right=0;
+            int matched=0;
+            int left_min=0;
+            int right_min=0;
+            int len_min=s.size()+10;
+            while(right < s.size() && left <s.size()){
+                //start from left
+                if(m[s[left]]==0){ // not matched
+                    left++; 
+                }else{ // left matched
+                    if(left>right)
+                        right = left;
+                    while(matched<t.size() && right < s.size()){ // right scan to the right
+                        if(m[s[right]]==1){
+                            if(n[s[right]]==0){ // if it's a newly covered number
+                                matched++; 
+                            }
+                            n[s[right]]++;
+                        }
+                        right++;
+                    }
+                    if(matched==t.size()){
+                        if(right-left+1 < len_min){ // found a shorter match, update!
+                            left_min = left;
+                            right_min = right;
+                            len_min =right-left+1;
+                        }
+                    }
+
+                    // left move on
+                    n[s[left]]--;
+                    if(n[s[left]]==0) // if deleted the last number
+                        matched--;
+                    left++; // move on;
+                }
+            }
+            if(left_min==0 && right_min==0)
+                return "";
+            else{
+                return s.substr(left_min, len_min);
+            }
         }
-    }
 };
 
 
 int main(){
-	Solution sol;
-	string s="hello";
-	string t="eo";
-	string out = sol.minWindow(s,t);
-	cout << out << endl;
-	return 1;
+    Solution sol;
+    string s="ADOBECODEBANC";
+    string t="ABC";
+    string out = sol.minWindow(s,t);
+    cout << out << endl;
+    return 1;
 }
