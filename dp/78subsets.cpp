3691e60325cd92034d2495d7aa2a2715d7d7c33e
chenruichou <chenrui.chou@gmail.com>
2016-01-23 00:46:10 -0800

    Added no.78

---
+++ dp/78subsets.cpp
@@ -0,0 +1,23 @@
+#include<vector.h>
+#include<iostream.h>
+using namespace std;
+
+class Solution{
+    public:
+        vector<vector<int>> subsets(vector<int> &nums){
+            subsets()
+        }
+        void subsets_sol(){
+        
+        }
+
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> out = sol.subsets(nums);
+    cout << out << endl;
+    return 1;
+}
+
