0c8d4338d9c52244ffc09b5f837c7bb9e1bbeed5
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2016-01-23 13:47:50 -0800

    Added no.78

--- dp/78subsets.cpp
+++ dp/78subsets.cpp
@@ -1,23 +1,39 @@
-#include<vector.h>
-#include<iostream.h>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 class Solution{
-    public:
-        vector<vector<int>> subsets(vector<int> &nums){
-            subsets()
-        }
-        void subsets_sol(){
-        
-        }
+	public:
+		vector<vector<int>> subsets(vector<int> &nums){
+			vector<vector<int>> out;
+			vector<int> curr;
+			subsets_sol(nums,0,curr,out);
+			return out;
+		}
+		void subsets_sol(vector<int> &nums, int i, vector<int> &curr, vector<vector<int>> &out){
+			if(i==nums.size()){
+				out.push_back(curr);
+				return;
+			}
+			curr.push_back(nums[i]);
+			subsets_sol(nums, i+1, curr, out);
+			curr.pop_back();
+			subsets_sol(nums, i+1, curr, out);
+			return;	
+		}
 
 };
 
 int main(){
-    Solution sol;
-    vector<int> nums = {1,2,3};
-    vector<vector<int>> out = sol.subsets(nums);
-    cout << out << endl;
-    return 1;
+	Solution sol;
+	vector<int> nums = {1,2,3,4};
+	vector<vector<int>> out = sol.subsets(nums);
+	for(int i=0;i<out.size();i++){
+		for(int j=0;j<out[i].size();j++)
+			cout << out[i][j] << " ";
+		cout << endl;
+	}
+
+	return 1;
 }
 
