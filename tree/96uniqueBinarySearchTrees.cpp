c853445c673e93bfecb2f97c35935d6d4155871c
Chen-rui Chou <cchou@Chen-ruideMacBook-Pro.local>
2016-01-31 16:41:44 -0800

    Added no.96

--- tree/96uniqueBinarySearchTrees.cpp
+++ tree/96uniqueBinarySearchTrees.cpp
@@ -1,29 +1,49 @@
 #include<iostream>
 #include<vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution{
-    public:
-        int numTrees(int n){
-            if(n==0)
-                return 1;
-            else if(n==1)
-                return 1;
-            else if (n==2)
-                return 2;
-            else{
-                int num=0;
-                for(int i=0;i<n;i++){
-                    num+=numTrees(n-i-1)*numTrees(i);
-                }
-                return num;
-            }
-        }
+	public:
+		int numTrees(int n){
+			unordered_map<int,int> memorized;
+			return numTreesSol(n, memorized);
+		}
+		int numTreesSol(int n, unordered_map<int,int> &memorized){
+			if(n==0)
+				return 1;
+			else if(n==1)
+				return 1;
+			else if (n==2)
+				return 2;
+			else{
+				int num=0;
+				for(int i=0;i<n;i++){
+					int out_i=0;
+					if(memorized[i]==0){
+						out_i = numTreesSol(i, memorized);
+						memorized[i] = out_i;
+					}else{
+						out_i = memorized[i];
+					}
+
+					int out_n_i_1=0;
+					if(memorized[n-i-1]==0){
+						out_n_i_1 = numTreesSol(n-i-1, memorized);
+						memorized[n-i-1] = out_n_i_1;
+					}else{
+						out_n_i_1 = memorized[n-i-1];
+					}
+					num+=out_n_i_1*out_i;
+				}
+				return num;
+			}
+		}
 };
 
 int main(){
-    Solution sol;
-    int out = sol.numTrees(3);
-    cout << out << endl;
-    return 1;
+	Solution sol;
+	int out = sol.numTrees(3);
+	cout << out << endl;
+	return 1;
 }
