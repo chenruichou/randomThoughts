c28885cb6b052b8365c6ab9da4c3d7b6676719a7
chenruichou <chenrui.chou@gmail.com>
2016-02-01 00:00:54 -0800

    Added no.102

---
+++ tree/102binaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+    public:
+        vector<vector<int>> levelOrder(TreeNode* root) {
+            vector<vector<int>> out;
+            vector<int> out_cand;
+            queue<TreeNode *> q;
+            queue<int> l;
+            int curr_level = 0;
+            q.push(root);
+            l.push(curr_level);
+            while(!q.empty()){
+                TreeNode *curr = q.front();
+                int level = l.front();
+                q.pop();
+                l.pop();
+                if(level>curr_level){
+                    out.push_back(out_cand);
+                    out_cand.clear();
+                    curr_level = level;
+                }
+                out_cand.push_back(curr->val);
+                if(curr->left){
+                    q.push(curr->left);
+                    level++;
+                    l.push(level);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                    l.push(level);
+                }
+            }
+        return out;
+        }
+};
+
+int main(){
+    Solution sol;
+    TreeNode *root = new TreeNode(2);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(0);
+    root->right->left = new TreeNode(2);
+    root->right->right = new TreeNode(4);
+    vector<vector<int>> out = sol.levelOrder(root);
+    for(int i=0;i<out.size();i++){
+        for(int j=0;j<out[i].size();j++){
+            cout << out[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 1;
+}
